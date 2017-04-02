fda8b6b906fef980681936b42ab8f66cdab5f999
jaymccoy300 <jaymccoy300@gmail.com>
2017-04-02 11:32:23 -0700

    pre assignment checkpoint 5

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,10 +5,11 @@
                 enabled: true,
                 requireBase: false
          });
-              $stateProvider
-                    .state('landing', {
-                        url: '/',
-                        templateUrl: '/templates/landing.html'
+      $stateProvider
+            .state('landing', {
+                url: '/',
+                controller: 'LandingCtrl as landing',
+                templateUrl: '/templates/landing.html'
          })
             .state('album', {
                   url: '/album',
@@ -16,6 +17,7 @@
          })
               .state('collection', {
                   url: '/collection',
+                  controller: 'CollectionCtrl as collection',
                   templateUrl: '/templates/collection.html'
          });
      }
