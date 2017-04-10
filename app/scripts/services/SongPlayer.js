7efa68eea0b261d733cd40873d4b7319508fe4b5
jaymccoy300 <jaymccoy300@gmail.com>
2017-04-10 07:17:06 -0700

    pre assignment checkpoint 10

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
  (function() {
- function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
      var SongPlayer = {};
 
      var currentAlbum = Fixtures.getAlbum();
@@ -28,6 +28,11 @@
         formats: ['mp3'],
         preload: true
     });
+          currentBuzzObject.bind('timeupdate', function() {
+              $rootScope.$apply(function() {
+                  SongPlayer.currentTime = currentBuzzObject.getTime();
+         });
+     });
  
     SongPlayer.currentSong = song;
  };  
@@ -46,6 +51,12 @@
         return currentAlbum.songs.indexOf(song);
  };
      SongPlayer.currentSong = null;
+     
+ /**
+ * @desc Current playback time (in seconds) of currently playing song
+ * @type {Number}
+ */
+    SongPlayer.currentTime = null;
      
      SongPlayer.play = function(song) {
          song = song || SongPlayer.currentSong;
@@ -91,12 +102,21 @@
             playSong(song);
      }
  };
-     
+     /**
+ * @function setCurrentTime
+ * @desc Set current time (in seconds) of currently playing song
+ * @param {Number} time
+ */
+ SongPlayer.setCurrentTime = function(time) {
+     if (currentBuzzObject) {
+         currentBuzzObject.setTime(time);
+     }
+ };
 
      return SongPlayer;
  }
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
